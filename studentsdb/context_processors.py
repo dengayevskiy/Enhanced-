8103eae394145503129327b03cd21aaf80d16b29
Denis Gayevskiy <dgaievskyi@playtika.com>
2015-07-06 19:15:01 +0300

    added default sort by last_name

--- studentsdb/context_processors.py
+++ studentsdb/context_processors.py
@@ -1,7 +1,5 @@
 __author__ = 'dgaievskiy'
 
-from django.http import HttpRequest
-
 
 def students_proc(request):
-    return {'PORTAL_URL': request.scheme + "://" + HttpRequest.get_host(request)}
+    return {'PORTAL_URL': request.scheme + "://" + request.get_host()}
